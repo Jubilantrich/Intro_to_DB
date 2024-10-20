c9bcab326f4188869b2d73c3c0c366e019165199
Jubilantrich <jubilantrich@gmail.com>
2024-10-20 08:32:03 +0000

    sql project

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -45,6 +45,6 @@ CREATE TABLE Order_Details (
     order_id INT,
     book_id INT,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES (order_id) ON DELETE CASCADE
-    FOREIGN KEY (book_id) REFERENCES (book_id) ON DELETE CASCADE
+    FOREIGN KEY (order_id) REFERENCES Orders (order_id) ON DELETE CASCADE
+    FOREIGN KEY (book_id) REFERENCES Books (book_id) ON DELETE CASCADE
 );
