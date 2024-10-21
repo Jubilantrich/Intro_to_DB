ebed129e6e316ba04bac800da8c2a52127b9243c
RICHARD DOK <jubilantrich@gmail.com>
2024-10-21 08:06:51 +0000

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -38,9 +38,9 @@ CREATE TABLE Orders(
 );
 
 CREATE TABLE Order_Details(
-    orderdetailid int(25) AUTO_INCREMENT PRIMARY KEY,
-    order_id int(25),
-    book_id Int(25),
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
     quantity DOUBLE NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
